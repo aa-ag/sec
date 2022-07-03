0cfe77a8697d6cc6d5ca72b0ea9afe0813fc5653
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-03 12:29:39 -0500

    Add comments & docstrings to two testing functions

--- testing.py
+++ testing.py
@@ -6,28 +6,38 @@ from pprint import pprint
 
 ############------------ FUNCTION(S) ------------##############################
 def get_ciks(headers):
+    '''
+     get ciks from sec & save them in a csv
+    '''
+    # set urls, headers & make a request
     url = "https://www.sec.gov/files/company_tickers.json"
     http_request = requests.get(url, headers=headers)
     response = http_request.json()
     
+    # normalize the response json
     df = pd.json_normalize(
         pd.json_normalize(
             response, max_level=0
         ).values[0]
     )
 
+    # fill each cik with zeroes to make sure each is 10 digits long
     df["cik_str"] = df["cik_str"].astype(str).str.zfill(10)
+    # make "title" the index (left-most)
     df.set_index("title", inplace=True)
-
+    # save the newly formatted dataframe into a csv
     df.to_csv("ciks.csv")
 
 
 def get_company_facts(cik, headers):
+    # set urls, headers & make a request
     api = "https://data.sec.gov/api/xbrl/"
     endpoint = f"companyfacts/CIK{cik}.json"
     url = api + endpoint
     http_request = requests.get(url, headers=headers)
+    # format the response as a json
     response = http_request.json()
+    # pretty-print the response
     pprint(response)
 
 
