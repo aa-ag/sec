87a9e1741995c57fa27948c13f442c64b42d7ce9
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-12 22:42:29 -0500

    Get finally working as expected, and pulling 10k text

--- testing.py
+++ testing.py
@@ -120,11 +120,13 @@ def parse_rss_feed():
 def a_ten_k(headers):
     '''
      try to get specific doc: 10k for a given company
-     check: https://gist.github.com/anshoomehra/ead8925ea291e233a5aa2dcaa2dc61b2
     '''
-    r = requests.get('https://www.sec.gov/Archives/edgar/data/320193/000032019318000145/0000320193-18-000145.txt')
+    r = requests.get(
+        'https://www.sec.gov/Archives/edgar/data/320193/000032019318000145/0000320193-18-000145.txt',
+        headers=headers
+    )
     data = r.text
-    print(data.text)
+    print(data)
 
 
 ############------------ DRIVER CODE ------------##############################
