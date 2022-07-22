15b9580beb50855c21f3332624140b278c1bc29e
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-21 21:17:46 -0500

    Opening each year's 10'k with a loop: pattern is wrong before year tho

--- testing.py
+++ testing.py
@@ -181,9 +181,10 @@ def get_past_ten_ks(cik, headers):
 
 def test_url_patterns():
     year = 2016
-    url = f'https://www.sec.gov/Archives/edgar/data/1318605/000156459017003118/tsla-10k_{year}1231.htm'
-    
-    webbrowser.open(url)
+    while year < 2022:
+        url = f'https://www.sec.gov/Archives/edgar/data/1318605/000156459017003118/tsla-10k_{year}1231.htm'
+        year += 1
+        webbrowser.open(url)
 
 ############------------ DRIVER CODE ------------##############################
 if __name__ == "__main__":
